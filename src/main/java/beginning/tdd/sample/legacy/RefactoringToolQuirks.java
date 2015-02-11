a524ca8f922bbab1db302c61c631327c3a11f484
Axle(이종화) <axle.lee@daumkakao.com>
2015-02-11 13:20:59 +0900

    add checkstyle plugin

--- src/main/java/beginning/tdd/sample/legacy/RefactoringToolQuirks.java
+++ src/main/java/beginning/tdd/sample/legacy/RefactoringToolQuirks.java
@@ -8,9 +8,7 @@ public class RefactoringToolQuirks {
 		return plus(someTwo);
 	}
 
-	/**
-	 * someOne이 인스턴스 변수이므로 어떤 메소드를 추출하든 간에 메서드는 x를 사용할 수 있기 때문
-	 */
+	// someOne이 인스턴스 변수이므로 어떤 메소드를 추출하든 간에 메서드는 x를 사용할 수 있기 때문
 	private int plus(int someTwo) {
 		return someOne + someTwo;
 	}
